9e89650f69df45c350f3f7fada3a0499a1195008
horoni <71940406+horoni@users.noreply.github.com>
2026-07-04 11:13:30 +0000

    test: add unhook sync test

---
+++ tests/test_unhook_sync.c
@@ -0,0 +1,62 @@
+/*
+ * Copyright (c) 2026 horoni (https://github.com/horoni)
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#include <assert.h>
+#include <stdatomic.h>
+#include <stdio.h>
+#include <threads.h>
+#include <unistd.h>
+
+#include "sighook.h"
+
+#define THREAD_COUNT 7
+#define ITERS 1000000
+
+volatile atomic_int g_hook_hits = 0;
+volatile atomic_int dummy = 0;
+
+__attribute__((noinline))
+void inc(void) {
+    atomic_fetch_add_explicit(&dummy, 1, memory_order_relaxed);
+}
+
+int worker(void *arg) {
+    (void)arg;
+    for(int i = 0; i<ITERS; ++i)
+        inc();
+    return 0;
+}
+
+void test_hook_cb(ucontext_t *uc) {
+    (void)uc;
+    atomic_fetch_add_explicit(&g_hook_hits, 1, memory_order_relaxed);
+}
+
+int main(void) {
+    assert(sg_init() == true);
+    assert(sg_inline((void *)inc, (hook_cb_t)test_hook_cb) == true);
+
+    thrd_t thrs[THREAD_COUNT];
+    for (int i = 0; i < THREAD_COUNT; ++i)
+        assert(thrd_create(&thrs[i], worker, NULL) == thrd_success);
+
+    usleep(100000);
+    
+    assert(sg_unhook((void *)inc) == true);
+
+    for (int i = 0; i < THREAD_COUNT; ++i) {
+        thrd_join(thrs[i], NULL);
+    }
+
+    int total_dummy = atomic_load(&dummy);
+    int total_hits = atomic_load(&g_hook_hits);
+
+    assert(total_dummy == THREAD_COUNT * ITERS);
+    assert(total_hits > 0);
+    assert(total_hits < total_dummy);
+    
+    printf("[OK] Unhook sync %d %d\n", total_dummy, total_hits);
+    return 0;
+}
